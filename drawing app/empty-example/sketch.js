ed0d85175a2c6fa2c4e2cc7a447e91f5de8c282b
Jonathan H <feteezuk@gmail.com>
2016-07-06 14:28:13 -0700

    1st Upload of Simple Drawing App

---
+++ drawing app/empty-example/sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+
+createCanvas(600,460);
+background(150,150,150);
+}
+
+function draw() {
+
+
+//head
+
+
+
+
+noStroke();
+fill(250,200,200,50);
+ellipse(mouseX,mouseY,90,25);
+
+
+
+}
+
+function mousePressed(){
+	background(150,150,150);
+	noStroke();
+fill(250,200,200,50);
+ellipse(mouseX,mouseY,90,25);
+
+
+}
